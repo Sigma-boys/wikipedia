47dfc3ae2ee1589398604f5f669de1e652674d83
hanssonsami <13hh1218@amal.se>
2025-02-14 15:07:47 +0100

    commmma

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log('DOM loaded with JavaScript');
     const chevron = document.querySelector('.welcome img.chevron');
     const dropdownMenu = document.querySelector('.dropdown-menu');
+    let isRotated = false;
 
     chevron.addEventListener('click', function() {
         dropdownMenu.style.display = dropdownMenu.style.display === 'block' ? 'none' : 'block';
+        if (isRotated) {
+            chevron.style.transform = 'rotate(0deg)';
+        } else {
+            chevron.style.transform = 'rotate(180deg)';
+        }
+        isRotated = !isRotated;
     });
 
     window.addEventListener('click', function(event) {
